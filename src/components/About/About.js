c506af3e176ace62e236db6948f8c5ca2d8204a8
Georgian Alin Alexandru <georgianalinalexandru@gmail.com>
2018-04-18 15:35:45 +0300

    new style for about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './About.css';
 import Typed from 'typed.js';
 import LinkButton from './../LinkButton';
@@ -11,10 +11,10 @@ class About extends Component {
     componentDidMount() {
         const options = {
             strings: ["Web", "Mobile"],
-            typeSpeed:200,
-            backSpeed:100,
+            typeSpeed: 200,
+            backSpeed: 100,
             backDelay: 500,
-            loop:true,
+            loop: true,
             cursorChar: '<span style="color: #EDB83D">|</span>',
         }
 
@@ -22,31 +22,52 @@ class About extends Component {
     }
 
     render() {
-        return(
+        return (
             <div>
-                <div className="text-center" style={{marginTop: "100px"}}>
-                    <h1 className="intro-text">Hi, I'm Alin. I build <br/> <span id='typed' ref={(el) => this.el = el}></span> applications</h1>
+                <div className="text-center" style={{ marginTop: "100px" }}>
+                    <div className="row display-flex">
+                        <div className="col-md-6 flex-container">
+                            <img src={me} style={{ maxHeight: '600px' }} className="img-responsive img-thumbnail center-image" alt="Alin Alexandru" />
+                        </div>
+                        <div className="col-md-6 flex-container align-text" style={{alignSelf: 'flex-end'}}>
+                            <h1 className="flash-yellow intro-text" style={{fontSize: '90px'}}>Hello</h1>
+                            <h1 className="intro-text" >I'm Alin. I build <br /> <span id='typed' ref={(el) => this.el = el}></span> applications</h1>
+                        </div>
+                    </div>
+
                 </div>
-                <br/>
-                <div id="about">
-                <div className="row text-center">
-                    <div className="col-md-12">
-                    <h3 className="title">About</h3>
-                        <p>I currently work as a Software Engineer at Luxoft, 
-                        the place where I consume my passion for development 
-                        and I am also student in my first year of master’s 
-                        degree learning Intelligent Transport Systems. 
+                <br />
+                <div id="about" style={{marginTop: '20px'}}>
+                    <div className="row text-center">
+                        <div className="col-md-12">
+
+                            <p>I currently work as a Software Engineer at Luxoft,
+                            the place where I consume my passion for development
+                            and I am also student in my first year of master’s
+                            degree learning Intelligent Transport Systems.
                         You can check my work history on  <a className="link-button" href="https://www.linkedin.com/in/georgian-alin-alexandru-5ab995119/">LinkedIn</a>.
-                        Outside of work, I like to hang out with my friends and travel. 
-                        If you have a project to be done, don't hesitate to <LinkButton to={'contact'} buttonName='contact me' style={{color: '#EDB83D', cursor: 'pointer'}} /> and get a coffee.</p>
-                    <img src={me} style={{maxHeight: '600px'}} className="img-responsive img-thumbnail center-image" alt="Alin Alexandru"/>
+                                Outside of work, I like to hang out with my friends and travel.
+                        If you have a project to be done, don't hesitate to <LinkButton to={'contact'} buttonName='contact me' style={{ color: '#EDB83D', cursor: 'pointer' }} /> and get a coffee.</p>
+
+                        </div>
                     </div>
                 </div>
-                </div>
-                <br/>
+                <br />
             </div>
         )
     }
+}
+
+const styles = {
+    hideHello: {
+        transform: 'translateY(-150%)',
+        transition: 'all 1s ease'
+    },
+    showHello: {
+        transform: 'translateZ(0)',
+        transition: 'all 1s ease'
+    },
+    
 }
 
 
